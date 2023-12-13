2f508e197bbc66c4ada34e169acc60bfbeba00be
virizar <virizar87@gmail.com>
2023-12-13 12:37:24 +0100

    Added  basic tests for parser

--- tests/test_json.nim
+++ tests/test_json.nim
@@ -1,18 +1,29 @@
-# This is just an example to get you started. You may wish to put all of your
-# tests into a single file, or separate them into multiple `test1`, `test2`
-# etc. files (better names are recommended, just make sure the name starts with
-# the letter 't').
-#
-# To run these tests, simply execute `nimble test`.
 
 import unittest
-
+import os
+import strutils
 import ngronpkg/parser
-test "parse json":
-    let file = "tests/resources/grep-separators.json"
+
+test "gron basic":
+    let file = "tests/resources/basic.json"
     let f =  open(file)
     defer: f.close()
-
     var parser = newJsonParser()
 
     parser.parse(f.readAll(), silent = false)
+
+test "gron validate":
+
+  var parser = newJsonParser()
+
+  let folder = "tests/resources/*"
+
+  for path in os.walkFiles(folder):
+
+    if not path.endsWith(".json"):
+      continue
+    
+    let f =  open(path)
+    defer: f.close()
+
+    parser.parse(f.readAll(), silent = true)
