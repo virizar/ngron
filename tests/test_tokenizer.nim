1fe6b1710658f12e31267b4cfdfd81ea8580cfcf
virizar <virizar87@gmail.com>
2024-07-18 11:33:02 +0200

    Added tokenizer tests

---
+++ tests/test_tokenizer.nim
@@ -0,0 +1,45 @@
+
+import unittest
+import os
+import strutils
+import ngronpkg/token
+import ngronpkg/tokenizer
+
+
+
+test "tokenizer basic json":
+  let inFilePath = "tests/resources/tokenizer.json"
+  let expectedFilePath = "tests/resources/tokenizer.json.out"
+  let inFile =  open(inFilePath)
+  let expectedFile = open(expectedFilePath)
+  defer: 
+    inFile.close()
+    expectedFile.close()
+  
+  let tokenizer  = newTokenizer(inFile.readAll())
+  
+  let tokens = tokenizer.tokenize()
+  
+  for i in 0..<tokens.len:
+    let token = tokens[i]
+    let expected = expectedFile.readLine()
+    check $token == expected
+
+test "tokenizer basic gron":
+  let inFilePath = "tests/resources/tokenizer.gron"
+  let expectedFilePath = "tests/resources/tokenizer.gron.out"
+  let inFile =  open(inFilePath)
+  let expectedFile = open(expectedFilePath)
+  defer: 
+    inFile.close()
+    expectedFile.close()
+  
+  let tokenizer  = newTokenizer(inFile.readAll())
+  
+  let tokens = tokenizer.tokenize()
+  
+  for i in 0..<tokens.len:
+    let token = tokens[i]
+    let expected = expectedFile.readLine()
+    check $token == expected
+
