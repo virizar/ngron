7f13f1b9c093a7d4ee4a8d9140e35e95fde66105
virizar <virizar87@gmail.com>
2024-07-18 11:31:00 +0200

    Added support for stdin input

--- src/ngron.nim
+++ src/ngron.nim
@@ -1,5 +1,7 @@
 import std/cmdline
+import std/os
+import std/terminal
 import ngronpkg/cli
 
 if isMainModule:
-  runCli(commandLineParams())
+  runCli(commandLineParams(), not isatty(stdin))
--- src/ngronpkg/cli.nim
+++ src/ngronpkg/cli.nim
@@ -1,41 +1,40 @@
 import std/cmdline
+import std/sequtils
 import std/options
 import json_parser
 import gron_parser
 import argparse
 
-# Transform JSON (from a file, URL, or stdin) into discrete assignments to make it greppable
-
-# Usage:
-#   gron [OPTIONS] [FILE|URL|-]
-
-# Options:
-#   -u, --ungron     Reverse the operation (turn assignments back into JSON)
-#   -v, --values     Print just the values of provided assignments
-#   -c, --colorize   Colorize output (default on tty)
-#   -s, --stream     Treat each line of input as a separate JSON object
-#   -k, --insecure   Disable certificate validation
-#   -j, --json       Represent gron data as JSON stream
-#       --no-sort    Don't sort output (faster)
-#       --version    Print version information
-
 var p = newParser:
   help("Transform JSON (from a file, URL, or stdin) into discrete assignments to make it greppable")
   flag( "--version", help="Print version information", shortcircuit=true)
   flag( "--validate", help="Validate json input. Will only print errors and warnings.")
-  flag( "--sort", help="sort keys (slower)")
+  flag( "-s", "--sort", help="sort keys (slower)")
   flag("-v", "--values", help="Print just the values of provided assignments")
   flag("-c", "--colorize", help="Colorize output")
   flag("-j", "--json-stream", help="Represent gron data as JSON stream")
   flag("-u", "--ungron", help="Reverse the operation (turn assignments back into JSON)")
   arg("input", help="Path to json file")
 
-proc runCli*(params : seq[string]) = 
+proc runCli*(params : seq[string], pipeInput : bool) = 
 
   try:
-    let opts = p.parse(params)
+    
+    var tempParams = params
 
-    let f =  open(opts.input)
+    if pipeInput:
+      tempParams = tempParams.filter(proc(x: string): bool = x.startsWith('-'))
+      tempParams.add("stdin")
+
+    let opts = p.parse(tempParams)
+
+    var f : File 
+    
+    if pipeInput:
+      f = stdin
+    else:
+      f = open(opts.input)
+
     defer: f.close()
     
     if opts.validate:
@@ -53,6 +52,13 @@ proc runCli*(params : seq[string]) =
     if err.flag == "argparse_help":
       echo err.help
       quit(1)
+    elif err.flag == "version":
+      echo "ngron 0.1.0"
+      quit(0)
+    else:
+      echo "Unknown flag"
+      quit(1)
+      
   except UsageError:
     stderr.writeLine getCurrentExceptionMsg()
     quit(1)
